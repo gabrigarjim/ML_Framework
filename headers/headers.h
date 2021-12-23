ba8add95ade4cd76a3b27d83a0b67a6cf9ced534
gabri <gabrigar96@gmail.com>
2021-12-23 11:46:31 +0100

    Removing trash

---
+++ headers/headers.h
@@ -0,0 +1,7 @@
+#include "TH1.h"
+#include "TCanvas.h"
+#include "TApplication.h"
+#include "TVector3.h"
+#include "TMath.h"
+#include <iostream>
+#include "TRandom.h"
--- python/python_course/strings.py
+++
@@ -1,4 +0,0 @@
-print("Hi man, python sucks, right? ")
-
-a = 'Python Sucks!'
-print('Test : ',a.startswith('Pyths'))
